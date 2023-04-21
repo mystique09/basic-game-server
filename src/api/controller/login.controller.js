2df2400360b8205136a8214c0e412fe35807b93e
mystique09 <knightkrypto7@gmail.com>
2023-04-21 22:06:04 +0800

    fix: method can't detect properties

--- src/api/controller/login.controller.js
+++ src/api/controller/login.controller.js
@@ -1,3 +1,5 @@
+const { LoginUserDTO } = require('../../domain/login');
+
 class LoginController {
   loginUsecase;
   env;
@@ -7,8 +9,22 @@ class LoginController {
     this.env = env;
   }
 
-  async login(req, res) {
-    return res.send('login');
+  login = async (req, res) => {
+    const payload = req.body;
+
+    if(!payload.username || !payload.password) {
+      return res.status(400).json({error: 'missing fields'});
+    }
+    
+    const user = new LoginUserDTO(payload.username, payload.password);
+    const query = await this.loginUsecase.checkUser(user.username);
+
+    if(query.length === 0) {
+      // TODO: save user to db
+      return res.status(404).json({ error: 'user not found'});
+    }
+    
+    return res.json({data: { token: 'randomtoken' }, message: 'logged in'});
   }
 }
 
