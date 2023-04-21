0fcde9da4420c0c518cb044c880571cf87c2a26c
mystique09 <knightkrypto7@gmail.com>
2023-04-21 12:31:22 +0800

    feat: init server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 const express = require('express');
-const server = express();
 const dotenv = require('dotenv');
+// bootstraps
+const env = require('./bootstrap/env');
+const db = require('./bootstrap/db');
+const server = require('./bootstrap/server');
+
+function main() {
+  const config = new env.Env();
+  const database = new db.Database({
+    host: config.db_host,
+    user: config.db_user,
+    password: config.db_pass,
+    database: config.db_name
+  });
+  const redis = new db.Redis(config.redis_url);
+
+  const app = new server.Server(express, config, database, redis);
+  app.start();
+}
+
+main();
