073c1bc1d2c4cc752ef24ebfe67f2ce91040077c
mystique09 <knightkrypto7@gmail.com>
2023-04-21 12:31:10 +0800

    add: server

---
+++ src/bootstrap/server.js
@@ -0,0 +1,26 @@
+class Server {
+  dbConnection;
+  redisClient;
+  config;
+  expressClient;
+  
+
+  constructor(express, config, dbConn, redisClient) {
+    this.config = config;
+    this.dbConnection = dbConn;
+    this.redisClient = redisClient;
+    this.serverConfig = config;
+    this.expressClient = express();
+  }
+
+  start() {
+    this.dbConnection.connect();
+    this.redisClient.connect();
+
+    this.expressClient.listen(this.config.port, () => {
+      console.log(`[INFO] Server listening on port: ${this.config.port} `);
+    });
+  }
+}
+
+module.exports = { Server };
