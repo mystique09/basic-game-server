96f3b0f53495004943b4c18706f1efdee8a8f3c3
mystique09 <knightkrypto7@gmail.com>
2023-04-21 22:05:34 +0800

    add: user repo

--- src/repository/user.js
+++ src/repository/user.js
@@ -1,16 +1,40 @@
+const { User } = require('../domain/user');
+
 class UserRepository {
-  constructor(dbConn, redisConn) {
-    this.dbConn = dbConn;
-    this.redisConn = redisConn;
+  constructor(db, redis) {
+    this.db = db;
+    this.redis = redis;
   }
 
-  create(payload) {}
+  create(payload) {
+    const newUser = new User(payload.username, payload.password);
+    this.db.conn.query('INSERT INTO users SET ?', newUser, function(err, results, fields) {
+      if (err) throw err;
+      console.log({results, fields});
+    });
+    return newUser;
+  }
 
-  getAll() {}
+  getAll() {
+    const users = this.db.conn.query('SELECT id, username, rank, score FROM users', function(err, results, fields) {
+      if (err) throw err;
+      console.log({results, fields});
+      return results;
+    });
+
+    return users;
+  }
 
   getOneById(id) {}
 
-  getOneByUsername(username) {}
+  getOneByUsername(username) {
+    return new Promise((res, rej) => {
+      const query = this.db.conn.query('SELECT * FROM users WHERE username = ?', username, function(err, results, fields) {
+        if (err) return rej(err);
+        return res(results);
+      });
+    });
+  }
 }
 
 module.exports = UserRepository;
