72d5f064e7bf6de2ef3967473e21fcd4cffbc6f5
mystique09 <knightkrypto7@gmail.com>
2023-04-22 10:09:25 +0800

    add: signup usecase

--- src/usecase/signup.js
+++ src/usecase/signup.js
@@ -1,11 +1,21 @@
+const { User } = require('../domain/user');
+
 class SignupUsecase {
   constructor(userRepository) {
     this.userRepo = userRepository;
   }
 
-  create(payload) {}
+  async create(payload) {
+    const newUser = new User(payload.userName, payload.userPwd);
+    const user = await this.userRepo.create(newUser);
+    return user; 
+  }
 
-  checkUser(username) {}
+  async checkUser(username) {
+    const user = await this.userRepo.getOneByUsername(username);
+    console.log(user);
+    return user.length === 1;
+  }
 }
 
 module.exports = SignupUsecase;
