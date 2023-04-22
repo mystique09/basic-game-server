d9b0726d7608fd10b4aa4934c4365e26ea59fda2
mystique09 <knightkrypto7@gmail.com>
2023-04-22 10:10:18 +0800

    add: game controller

--- src/api/controller/game.controller.js
+++ src/api/controller/game.controller.js
@@ -7,12 +7,59 @@ class GameController {
     this.env = env;
   }
 
-  saveScore = (req, res) => {
-    return res.send('save score');
+  saveScore = async (req, res) => {
+    try {
+      const payload = req.body;
+
+      if(!payload.token || !payload.myScore) {
+        return res.status(400).json({code: 400, msg: 'missing fields'});
+      }
+      
+      if(isNaN(Number(payload.myScore))) {
+        return res.status(500).json({code: 500, msg: "bad score"});
+      }
+      
+      const validSession = await this.gameUsecase.checkSession(payload.token);
+      
+      if(!validSession) {
+        return res.status(500).json({code: 500, msg: "bad token"});
+      }
+      
+      const saveScore = await this.gameUsecase.saveScore(payload.token, payload.myScore);
+      console.log({saveScore});
+      return res.status(200).json({code: 200, msg: "OK"});
+    } catch(err) {
+      console.log({err})
+      return res.status(500).json({code: 500, msg: "something went wrong"});
+    }
   }
 
-  leaderBoard = (req, res) => {
-    return res.send('leader board');
+  leaderBoard = async (req, res) => {
+    try {
+      const payload = req.body;
+
+      if(!payload.token) {
+        return res.status(400).json({code: 400, msg: 'missing fields'});
+      }
+
+      const validSession = await this.gameUsecase.checkSession(payload.token);
+      if(!validSession) {
+        return res.status(500).json({code: 500, msg: "bad token"});
+      }
+
+      const userData = await this.gameUsecase.getSession(payload.token);
+      const board = await this.gameUsecase.leaderBoard();
+    
+      return res.status(200).json({
+        data: {
+          mine: JSON.parse(userData),
+          rank: board
+        }
+      });
+    } catch(e) {
+      console.log(e)
+      return res.status(500).json({code: 500, msg: "something went wrong"});
+    }
   }
 }
 
