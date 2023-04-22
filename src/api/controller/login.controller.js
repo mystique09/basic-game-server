c4d94e76976dd70bd7f42ae9bf711ce5af29519a
mystique09 <knightkrypto7@gmail.com>
2023-04-22 10:10:07 +0800

    feat: login controller

--- src/api/controller/login.controller.js
+++ src/api/controller/login.controller.js
@@ -10,21 +10,37 @@ class LoginController {
   }
 
   login = async (req, res) => {
-    const payload = req.body;
+    try {
+      const payload = req.body;
 
-    if(!payload.username || !payload.password) {
-      return res.status(400).json({error: 'missing fields'});
-    }
+      if(!payload.userName || !payload.userPwd) {
+        return res.status(400).json({code: 400, error: 'missing fields'});
+      }
     
-    const user = new LoginUserDTO(payload.username, payload.password);
-    const query = await this.loginUsecase.checkUser(user.username);
+      const user = new LoginUserDTO(payload.userName, payload.userPwd);
+      const checkUser = await this.loginUsecase.checkUser(user.username);
+      if(checkUser.length === 0) {
+        return res.status(500).json({code: 500, msg: "login failed"});
+      }
 
-    if(query.length === 0) {
-      // TODO: save user to db
-      return res.status(404).json({ error: 'user not found'});
+      const validPassword = this.loginUsecase.comparePassword(user.password, checkUser[0].password);
+
+      if(!validPassword) {
+        return res.status(500).json({code: 500, error: "login failed"});
+      }
+
+      const randomToken = this.loginUsecase.createToken();
+      const session = {
+        username: checkUser[0].username,
+        rank: checkUser[0].rank,
+        score: checkUser[0].score
+      };
+      const saveSession = await this.loginUsecase.saveSession(randomToken, session);
+         
+      return res.status(200).json({code: 200, randomToken});
+    } catch(e) {
+      return res.status(500).json({code: 500, error: "login failed"});
     }
-    
-    return res.json({data: { token: 'randomtoken' }, message: 'logged in'});
   }
 }
 
