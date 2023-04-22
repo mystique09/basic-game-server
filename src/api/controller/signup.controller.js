98eef112dd7f6a3fc415945f8b6a8d16d1b2f1e8
mystique09 <knightkrypto7@gmail.com>
2023-04-22 10:09:57 +0800

    feat: signup controller

--- src/api/controller/signup.controller.js
+++ src/api/controller/signup.controller.js
@@ -7,8 +7,28 @@ class SignupController {
     this.env = env;
   }
 
-  newAccount = (req, res) => {
-    return res.send('new account');
+  newAccount = async (req, res) => {
+    try {
+      const payload = req.body;
+
+      if(!payload.userName || !payload.userPwd) {
+        return res.status(400).json({code: 400, msg: "missing fields"});
+      }
+
+      let user = await this.signupUsecase.checkUser(payload.userName);
+      console.log(user)
+      if(user){
+        return res.status(500).json({code: 500, msg: "account already exist"});
+      }
+
+      let newUser = await this.signupUsecase.create(payload);
+
+      console.log({newUser});
+      
+      return res.status(200).json({code: 200, msg: "OK"});
+    } catch(e) {
+      return res.status(500).json({code: 500, msg: "account already exist"});
+    }
   }
 }
 
